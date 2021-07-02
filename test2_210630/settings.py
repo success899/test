9d89e0da0e0b26f69afbca73ae27a3c10dfefa3b
YJH <success899@naver.com>
2021-07-02 11:59:21 +0900

    210702 test commit

--- test2_210630/settings.py
+++ test2_210630/settings.py
@@ -14,9 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 env_list = dict()
-
 local_env = open(os.path.join(BASE_DIR, '.env'))
 
 while True:
