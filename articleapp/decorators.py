651e04891f1efb765dca57ee67b62bc9678b84b3
YJH <success899@naver.com>
2021-08-04 18:11:25 +0900

    210804 test commit 4

---
+++ articleapp/decorators.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponseForbidden
+
+from articleapp.models import Article
+
+
+def article_ownership_required(func):
+    def decorated(requset, *args, **kwargs):
+        target_article = Article.objects.get(pk=kwargs['pk'])
+
+        if target_article.writer == requset.user:
+            return func(requset, *args, **kwargs)
+
+        else:
+            return HttpResponseForbidden()
+
+    return decorated
