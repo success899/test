fb877bdbd71a3b089c6a6b3ec8805cfee225cfb3
YJH <success899@naver.com>
2021-08-25 17:05:36 +0900

    210825 test commit 2

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -12,27 +14,34 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
+@transaction.atomic
+def db_transaction(user, article):
+    like_record = LikeRecord.objects.filter(user=user, article=article)
+
+    if like_record.exists():
+        # 좋아요 반영 안되는 부분
+        raise ValidationError('like already exists')
+    else:
+        # 좋아요 반영 되는 부분
+        LikeRecord(user=user, article=article).save()
+    article.like += 1
+    article.save()
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
 
     def get(self, request, *args, **kwargs):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
-
-        like_record = LikeRecord.objects.filter(user=user, article=article)
-
-        if like_record.exists():
-            # 좋아요 반영 안되는 부분
+        try:
+            db_transaction(user, article)
+            # 좋아요 반영 O
+            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+        except ValidationError:
+            # 좋아요 반영 X
             messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']}))
-
-        else:
-            # 좋아요 반영 되는 부분
-            LikeRecord(user=user, article=article).save()
-
-        article.like += 1
-        article.save()
-        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
 
         return super().get(request, *args, **kwargs)
 
