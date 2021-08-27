9f249b5030ba9b37b1ffedd6500c26d2e0379f6d
YJH <success899@naver.com>
2021-08-27 11:18:57 +0900

    210827 test commit 1

--- test2_210630/settings.py
+++ test2_210630/settings.py
@@ -164,5 +164,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = reverse_lazy('accounts:hello_world')
+LOGIN_REDIRECT_URL = reverse_lazy('articleapp:list')
 LOGOUT_REDIRECT_URL = reverse_lazy('accounts:login')
