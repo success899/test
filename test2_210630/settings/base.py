d3d5486f3044968dc2f15ca6651e1270ce215f04
YJH <success899@naver.com>
2021-09-28 13:04:41 +0900

    210928 test commit 7

--- test2_210630/settings/base.py
+++ test2_210630/settings/base.py
@@ -16,31 +16,6 @@ from pathlib import Path
 from django.urls import reverse_lazy
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-env_list = dict()
-local_env = open(os.path.join(BASE_DIR, '.env'))
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qlph7=zzw@%8qav-+s+wxzmnnaije&d074+jo%ft4mrr8ope+^'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
-
 
 # Application definition
 
